f5c797966138d9b86aad64828adabcf6db62377a
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-11-27 12:21:00 -0500

    added starter code

---
+++ Codio/creatingABootstrapNavigationBar/routing.js
@@ -0,0 +1,12 @@
+// This is a function that takes a page path as an argument
+// You need to make the function load the content of that page into the "content" div on the index file
+async function loadPage(page) {
+  // fetch the page, use await
+  const res;
+  // get text from res, use await
+  const content;
+  // get the element with id 'content'
+  const element;
+  // set innerHTML of the element
+  // your code goes here
+}
